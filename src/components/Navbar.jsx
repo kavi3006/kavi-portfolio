26bc6296e9fc560608ec3b89ba48885492cb8b0e
kavi3006 <kavi3006@hotmail.com>
2025-05-26 12:38:30 +0530

    Fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
         <a href="#about">About</a>
         <a href="#experience">Experience</a>
         <a href="#projects">Projects</a>
-        <a href="./public/Kavi_Resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
+        <a href="./public/Resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
         <a href="#contact">Contact</a>
       </div>
       <div className="menu-toggle" id="mobile-menu">
